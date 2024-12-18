26835392cd53fa67e3277f278cdc0907d5b5c65d
xiongxc <xuecheng.xiong@spirent.com>
2024-12-18 08:33:15 +0000

    [Release v1.2] 1.Add support for set different rest session name in multi 2.Add otgService**.sh in default folder.
    instance

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@
 echo "Starting entrypoint script"
 
 # Check if LABSERVER is set, otherwise use default value
-LABSERVER=${LABSERVER:-"10.66.240.179"}
+LABSERVER=${LABSERVER:-"10.109.125.126"}
 echo "Using LABSERVER: $LABSERVER"
 
+CONTAINER_ID=$(cat /proc/self/cgroup | grep 'memory' | awk -F'/' '{print $3}' | head -n 1)
+echo "Container ID: $CONTAINER_ID"
+
 # Construct the command with the expanded LABSERVER variable
-CMD="/opt/ondatraOTG/otgservice.V1.0.*.sh --target otgservice"
+CMD="/opt/ondatraOTG/otgservice.V1.*.sh --target otgservice"
 
 # Run the constructed command
 echo "Running otgservice script with command: $CMD"
@@ -17,6 +20,8 @@ cd /opt/ondatraOTG/otgservice || exit 1
 ./otgctl --start
 sleep 2
 ./otgctl --restserver $LABSERVER
+sleep 2
+./otgctl --session --with-username admin --with-session-name ${CONTAINER_ID:0:12}
 status=$?
 
 if [ $status -ne 0 ]; then
